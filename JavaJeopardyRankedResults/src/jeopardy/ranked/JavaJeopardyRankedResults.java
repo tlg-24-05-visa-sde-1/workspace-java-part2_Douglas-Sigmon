a6b0098ce98ddb54f16191ca57e71ab6f40bb772
Douglas Sigmon <douglass@tlgcohort.com>
2024-06-12 08:51:28 -0500

    cleaned up

--- JavaJeopardyRankedResults/src/jeopardy/ranked/JavaJeopardyRankedResults.java
+++ JavaJeopardyRankedResults/src/jeopardy/ranked/JavaJeopardyRankedResults.java
@@ -3,16 +3,13 @@ package jeopardy.ranked;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 class JavaJeopardyRankedResults {
 
