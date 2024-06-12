49e73346bb55d7a2817b15b58e64f8c945b99139
Douglas Sigmon <douglass@tlgcohort.com>
2024-06-12 11:48:32 -0500

    iteration 4: save and load Board object from binary file board.dat

--- DuckRace/src/com/duckrace/Board.java
+++ DuckRace/src/com/duckrace/Board.java
@@ -1,6 +1,11 @@
 package com.duckrace;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
@@ -42,13 +47,37 @@ import java.util.TreeMap;
  *   17       17    Dom        1    DEBIT_CARD
  */
 
-public class Board {
+
+public class Board implements Serializable {
+  private static final String DATA_FILE_PATH = "data/board.dat";
+  private static final String STUDENT_ID_FILE_PATH = "conf/student-ids.csv";
 
   private final Map<Integer, String> studentIdMap = loadStudentIdMap();
   private final Map<Integer, DuckRacer> racerMap = new TreeMap<>();
 
-  //TESTING PURPOSES ONLY
+  //prevents the instantiation from outside.
+  private Board() {
+  }
 
+  /*
+   * Read from binary file data/board.dat or create new Board (if file not there).
+   * NOTE: new Board object only created the *very first time* the app is run.
+   */
+  public static Board getInstance() {
+    Board board = null;
+    if (Files.exists(Path.of(DATA_FILE_PATH))) {
+      try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(DATA_FILE_PATH))) {
+        board = (Board) in.readObject();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    } else { //only happens the very first time we run the app
+      board = new Board();
+    }
+    return board;
+  }
+
+  //TESTING PURPOSES ONLY
   public int maxId() {
     return studentIdMap.size();
   }
@@ -84,24 +113,36 @@ public class Board {
       racerMap.put(id, racer);
     }
     racer.win(reward);
+    save();
+  }
+
+  /*
+   * Writes 'this' Board object to binary file data/board.dat.
+   * In more detail, we are using Java's built-in Object Serialization facility
+   * to write the state of this object to the file.
+   */
+  private void save() {
+    try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(DATA_FILE_PATH))) {
+      out.writeObject(this); //write "me" im a board object to the file.
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 
   //Populates sutdentIdeMpa from file conf/student-ids.csv
   private Map<Integer, String> loadStudentIdMap() {
-    Map<Integer, String> map = new HashMap<Integer, String>();
+    Map<Integer, String> map = new HashMap<>();
     try {
-      List<String> lines = Files.readAllLines(Path.of("conf/student-ids.csv"));
+      List<String> lines = Files.readAllLines(Path.of(STUDENT_ID_FILE_PATH));
       // for each line(String), we need to split it into "tokens"
       for (String line : lines) {
         String[] tokens = line.split(",");
         map.put(Integer.valueOf(tokens[0]), tokens[1]);
-
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
-
     return map;
   }
 
