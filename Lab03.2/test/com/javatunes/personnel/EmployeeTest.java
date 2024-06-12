d83a4931f25649b21701215b697eaf6a3927501e
Douglas Sigmon <douglass@tlgcohort.com>
2024-06-12 14:54:00 -0500

    used anonymous class for mocking Employee for equals test

--- Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
@@ -7,31 +7,59 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class EmployeeTest {
-  private Employee emp;
+  private Employee emp1;
   private Employee emp2;
   @Before
   public void setUp() {
-    emp = new SalariedEmployee("John", Date.valueOf("2000-01-01"));
-    emp2 = new SalariedEmployee("John", Date.valueOf("2000-01-01"));
+    emp1 = getEmployee();
+    emp2 = getEmployee();
+  }
 
+  private static Employee getEmployee() {
+    return new Employee("John", Date.valueOf("2001-01-01")) {
+      public double pay() {return 0;}
+
+      @Override
+      public double payTaxes() {return 0;}
+    };
   }
 
   @Test
   public void equals_shouldReturnFalse_differentName_sameHireDate() {
     emp2.setName("Mary");
-    assertNotEquals(emp, emp2);
+    assertNotEquals(emp1, emp2);
 
   }
 
   @Test
   public void equal_ShouldReturnTrue_allPropertiesSame() {
-    assertEquals(emp, emp2); // does an .equals() check
+    assertEquals(emp1, emp2); // does an .equals() check
   }
 
   @Test
   public void equals_shouldReturnFalse_sameName_differentHireDate() {
     emp2.setHireDate(Date.valueOf("2020-02-02"));
-    assertNotEquals(emp, emp2); // does a !.equals
+    assertNotEquals(emp1, emp2); // does a !.equals
 
   }
+  //NAMED MEMBER-LEVEL INNER CLASSES
+//  private class DummyEmployee extends Employee {
+//
+//    public DummyEmployee() {
+//    }
+//
+//    public DummyEmployee(String name, Date hireDate) {
+//      super(name, hireDate);
+//    }
+//
+//    @Override
+//    public double pay() {
+//      return 0;
+//    }
+//
+//    @Override
+//    public double payTaxes() {
+//      return 0;
+//    }
+//  }
 }
